a477f2f6b145c0340a32285220a5a12153a2c4f9
shulianghe <nevergdy@gmail.com>
2013-04-09 14:14:01 +0800

    add device to sqlite

--- pro/nmap.sh
+++ pro/nmap.sh
@@ -8,8 +8,8 @@
 #nmap.sh:嗅探局域网内活跃ip并将他们写入iplist.txt
 
 #按语言得不同选择
-#ip=`ifconfig eth0 |grep 'inet 地址'|awk -F '  *|:' '{print $4}'`
-ip=`ifconfig eth0 |grep 'inet addr'|awk -F '  *|:' '{print $4}'`
+ip=`ifconfig eth0 |grep 'inet 地址'|awk -F '  *|:' '{print $4}'`
+#ip=`ifconfig eth0 |grep 'inet addr'|awk -F '  *|:' '{print $4}'`
 #echo "Host ip = $ip"
 
 #nmap -sP "$ip"/24 >tmp.txt
@@ -28,15 +28,12 @@ echo "device ip is $deviceIP"
 
 rm tmp.txt
 
-#创建设备列表
 sqlite3 device.db "create table devicelist(deviceName text,deviceIP VARCHAR(100))"
 
-#获取数据库中已经存在的设备信息
 sqlite3 device.db "select * from devicelist" > existIP.txt
 existIP=`grep -Eo '([0-9]{3}).([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3})' existIP.txt`
 rm existIP.txt
 
-#将已经存在的设备信息与新扫描到的设备IP比较，若已存在则不添加
 name=camera
 i=1
 #for ip in $deviceIP
@@ -53,14 +50,17 @@ do
 	do
 		if [ "$newIP"x = "$oldIP"x ]; then
 			echo "IP '$newIP' already exist"
-			flag=1
+			((i++))
+            flag=1
 			break
 		fi
+        ((i++))
 	done
 	if [ $flag -eq 0 ];then
 		sqlite3 device.db "insert into devicelist values('$name$i','$newIP')"
-	fi 
-	((i++))
+	    ((i++))
+    fi 
+#	((i++))
 #	echo $newIP 
 done
 
